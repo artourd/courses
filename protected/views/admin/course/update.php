eb490c3e4e18cb4d3ce407d77faae05ebbd23907
artourd <r-2@rambler.ru>
2014-04-20 16:18:29 +0300

    исправление ошибок

--- protected/views/admin/course/update.php
+++ protected/views/admin/course/update.php
@@ -18,4 +18,4 @@ $this->menu=array(
 
 <h1>Update Course <?php echo $model->id; ?></h1>
 
-<?php $this->renderPartial('_form', array('model'=>$model)); ?>
+<?php $this->renderPartial('_form', array('model'=>$model, 'products' => $products)); ?>
